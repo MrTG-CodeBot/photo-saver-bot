c7cc9839f845673ea1cdc98d41dde30b9e0814e3
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2024-05-17 19:24:56 +0530

    Create info.py

---
+++ info.py
@@ -0,0 +1,52 @@
+import re
+import os
+from os import environ
+from pyrogram import enums
+import asyncio
+import json
+from pyrogram import Client
+
+id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.strip().lower() in ["on", "true", "yes", "1", "enable", "y"]: return True
+    elif value.strip().lower() in ["off", "false", "no", "0", "disable", "n"]: return False
+    else: return default
+
+API_ID = int(os.environ.get('API_ID', ''))
+API_HASH = os.environ.get('API_HASH', '')
+
+# for the main bot
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
+
+# for the photo saver bot
+BOT_TOKEN_2 = os.environ.get('BOT_TOKEN', '')
+PORT = os.environ.get("PORT", "8080")
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', ']').split()]
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
+REQUESTED_CHANNEL = int(os.environ.get("REQUESTED_CHANNEL", ""))
+MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "False")), False)
+ADMIN_CHANNEL_ID = int(os.environ.get("ADMIN_CHANNEL_ID", ""))
+EVAL_ID = int(os.environ.get("EVAL_ID", ""))
+PIC_LOG_CHANNEL = int(os.environ.get("PIC_LOG_CHANNEL", ""))
+
+# important information for your bot
+S_GROUP = environ.get('S_GROUP', "https://t.me/sdbots_support")
+S_CHANNEL = environ.get('S_CHANNEL', "https://t.me/sd_bots")
+
+F_SUB = os.environ.get("FORCE_SUB", "sd_bots") 
+
+# for mongodb and for the main bot
+DATABASE_NAME = os.environ.get("DB_NAME", "")     
+DATABASE_URI  = os.environ.get("DB_URL", "")
+MONGO_URL = os.environ.get('MONGO_URL', "")
+
+# for the photo saver bot
+DATABASE_NAME_2 = os.environ.get("DATABASE_NAME_2", "")
+DATABASE_URI_2 = os.environ.get("DATABASE_URI_2", "")
+
+#for spotify 
+SPOTIFY_CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_ID', 'd3a0f15a75014999945b5628dca40d0a')
+SPOTIFY_CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET', 'e39d1705e35c47e6a0baf50ff3bb587f')
+
+#for google
+G_API_KEY = os.environ.get('G_API_KEY','AIzaSyAGv5kIu2-E0N9eTdK7lzevl2nr3sOk6is')
